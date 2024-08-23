0783bf14b052f6b7054ec3d920aeec2bf62b8bd3
Taira <sa.hirao1962@gmail.com>
2024-08-23 10:40:10 +0900

    kadai_005を修正アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,8 +4,12 @@ let weight =68;
 // 変数の宣言、代入
 let height =1.7;
 
+// 変数の宣言、代入
+let BMI = (weight / (height * height));
+
+
 console.log (weight);
 console.log (height);
 
 // 計算式
-console.log ( weight / (height * height));
+console.log  (BMI);
